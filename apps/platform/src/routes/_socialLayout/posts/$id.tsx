3695b961d9c7003ca01aa32c4babb460323e38ac
Indra Zulfi Mushoddaq <me@indrazm.com>
2025-11-03 09:25:43 +0700

    adding admin badge

--- apps/platform/src/routes/_socialLayout/posts/$id.tsx
+++ apps/platform/src/routes/_socialLayout/posts/$id.tsx
@@ -68,11 +68,32 @@ function PostDetail() {
 						className="group"
 					>
 						<div className="space-y-0.5">
-							<div className="group-hover:underline">
-								{post.user.name || post.user.email}
+							<div className="flex gap-1 items-center group-hover:underline">
+								<div>{post.user.name || post.user.email}</div>
+								{post.user.role === "admin" && (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="1.1em"
+										height="1.1em"
+										viewBox="0 0 24 24"
+										className="mt-1 bg-gradient-to-r from-primary to-yellow-500 bg-clip-text text-transparent"
+									>
+										<title>verified</title>
+										<defs>
+											<linearGradient x1="0%" y1="0%" x2="100%" y2="0%">
+												<stop offset="0%" stopColor="rgb(234 125 8)" />
+												<stop offset="100%" stopColor="rgb(234 179 8)" />
+											</linearGradient>
+										</defs>
+										<path
+											fill="url(#verifiedGradient)"
+											d="m8.6 22.5l-1.9-3.2l-3.6-.8l.35-3.7L1 12l2.45-2.8l-.35-3.7l3.6-.8l1.9-3.2L12 2.95l3.4-1.45l1.9 3.2l3.6.8l-.35 3.7L23 12l-2.45 2.8l.35 3.7l-3.6.8l-1.9 3.2l-3.4-1.45zm2.35-6.95L16.6 9.9l-1.4-1.45l-4.25 4.25l-2.15-2.1L7.4 12z"
+										/>
+									</svg>
+								)}
 							</div>
 							<p className="text-foreground/50 text-xs">
-								{post.user.bio || post.user.username}
+								{post.user.bio || `@${post.user.username}`}
 							</p>
 						</div>
 					</Link>
